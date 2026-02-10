51592075f12207acc580385c6f18071d1941634e
Zalam2004 <dal638779@utdallas.edu>
2026-02-10 02:37:43 +0000

    Mouseover and out effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 // main.js
-
-console.log("Hello, World!");
+let targetElement = document.querySelector("h2");
+console.log(targetElement.outerHTML);
+targetElement.addEventListener("mouseover", funtion () {
+    console.log(this.innerText);
+    this.innerText = "TIME";
+    this.style = "cursor: wait";
+});
+targetElement.addEventListener("mouseout", funtion () {
+    this.innerText = "IKEA";
+    this.style = "cursor: pointer";
+});
