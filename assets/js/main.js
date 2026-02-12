a09876257b04113f34b7a86437dafdb03c81c0fc
Zalam2004 <dal638779@utdallas.edu>
2026-02-12 02:46:37 +0000

    Added Modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,6 @@ let targetElement = document.querySelector("h2");
 console.log(targetElement.outerHTML);
 targetElement.addEventListener("mouseover", funtion () {
     console.log(this.innerText);
-    this.innerText = "TIME";
-    this.style = "cursor: wait";
 });
 targetElement.addEventListener("mouseout", funtion () {
-    this.innerText = "IKEA";
-    this.style = "cursor: pointer";
 });
